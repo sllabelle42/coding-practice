941ced25514dd2742d42f5873ea87acbc97d53a5
sllabelle42 <111803969+sllabelle42@users.noreply.github.com>
2023-01-23 14:06:04 -0600

    Started Sorting algorithm program

---
+++ practice_sorting_insertion/Driver.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "InsertionSort.h"
+using namespace std;
+
+//This program sorts numbers taken from user input
+
+int main()
+{
+   numbers* numArray = new numbers[150000];
+   int nunNUmbers = 0;
+   int temp;
+   int num;
+
+   
+    
+}
---
+++ practice_sorting_insertion/InsertionSort.h
@@ -0,0 +1,14 @@
+/*
+    Filename: InsertionSort.h
+    Author: Serena LaBelle
+    Date: 01/23/2023
+    Purpose: Provides function to sort numbers using insertion sort.
+*/
+
+struct numbers
+{
+    int number;
+    int count;
+};
+
+void insertionSort(numbers *numArray, int numWords);
